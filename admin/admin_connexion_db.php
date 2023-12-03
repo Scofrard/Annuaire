e7e7f3531bcb61ef5dbc7544d7182288d31ebdb2
Julienpetit951 <julienpetit951@gmail.com>
2023-12-03 19:38:56 +0100

    connexion_db

--- admin/admin_connexion_db.php
+++ admin/admin_connexion_db.php
@@ -1,11 +1,17 @@
 <?php
+// Définition des variables de connexion
+$host = "localhost:8889"; // Ou 3307 si vous changez le port, par exemple
+$dbname = "annuaire";
+$username = "root";
+$passwordbdd = "root";
+$dsn = "mysql:host=" . $host . ";dbname=" . $dbname;
+
+// Initialisation de la connexion
 try {
-    $bdd = new PDO('mysql:host=localhost;dbname=annuaire', 'root', '');
-    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_WARNING); // On émet une alerte à chaque fois qu'une requête a échoué.
-    global $bdd; //On défini notre variable globale
-}
-//retourne un message d'erreur lorsqu'une
-// exception est levée
-catch (\Exception $e) {
-    echo $e->getMessage();
+    $bdd = new PDO($dsn, $username, $passwordbdd);
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connexion établie"; //Vérifier que la connexion est établie
+} catch (PDOException $e) {
+    //Indiquer s'il y a une erreur de connexion
+    echo "Erreur de connexion : " . $e->getMessage();
 }
